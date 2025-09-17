7cb19cd708e290e242a7c1c9f6a4e6b0ddb0de3e
topg85 <danielcarson1985@gmail.com>
2025-09-17 14:53:21 +0100

    There's methods in our models

--- blog/models.py
+++ blog/models.py
@@ -14,10 +14,10 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["-created_on", "author"]
 
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return f"The title of this post is {self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
