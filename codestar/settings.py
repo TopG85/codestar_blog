9df9a9cf0ed6696dc0138a7244ab654caf8cd637
topg85 <danielcarson1985@gmail.com>
2025-09-22 17:39:55 +0100

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL") , conn_max_age=600)
 }
 
 CSRF_TRUSTED_ORIGINS = [
